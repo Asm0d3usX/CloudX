58bfe91bb91f145366081664e7498bf6d9abf245
Asm0d3usX <117481415+Asm0d3usX@users.noreply.github.com>
2025-11-08 02:28:53 +0800

    Add files via upload

---
+++ Sarangfilm/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    description = "Sarangfilm adalah plugin untuk streaming film dan serial."
+    language = "id"
+    authors = listOf("Asm0d3usX")
+    status = 1
+    isCrossPlatform = true
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "AsianDrama",
+    )
+}
