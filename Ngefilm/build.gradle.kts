ca13ac959bcb90926a4e04b4c2cfebc759ebf132
Asm0d3usX <117481415+Asm0d3usX@users.noreply.github.com>
2025-11-08 23:46:40 +0800

    Add files via upload

---
+++ Ngefilm/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 1
+cloudstream {
+    description = "Ngefilm - Nonton movie dengan sub indo"
+    language = "id"
+    authors = listOf("Duro92", "Asm0d3usX")
+	isCrossPlatform = true
+    status = 1
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Movie",
+    )
+}
---
+++ Nomat/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    description = "Nomat - Streaming film series dan movie sub indo"
+    language = "id"
+    authors = listOf("Duro92", "Asm0d3usX")
+	isCrossPlatform = true
+    status = 1
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Movie",
+    )
+}
